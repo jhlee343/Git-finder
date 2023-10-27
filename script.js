3fbb09878b2a1ff6d1d55fa627cc3b84e3dd6182
reenact_l <77737814+jhlee343@users.noreply.github.com>
2023-10-27 17:40:23 +0900

    modify javascript

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
-function search() {
-    let search=document.getElementById("search-id")
+function search(e) {
+    var text = document.getElementById("search").value;
+    if(e.keyCode == 13){
+        e.preventDefault();
+        getSearch();
+    }
+}
+
+async function getSearch (){
+     const response = await (await fetch("https://api.github.com/${text}/repos")).json() ;
 }
